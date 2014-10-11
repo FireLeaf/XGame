54cf245b6989b3c66565e147e86b4820fc6dd630
qfsly <ycqfsly@163.com>
2014-10-11 16:47:25 +0800

    terrain modify

--- XGame/XSrc/XEngine/XTerrain/include/XTerrain.h
+++ XGame/XSrc/XEngine/XTerrain/include/XTerrain.h
@@ -12,14 +12,40 @@
 /*******
 
 *******/
+struct XAnchorPos
+{
+	int x, y, z;//暂不用z
+};
+
+struct XTerrainVertex 
+{
+	XVector3 pos;
+	XVector3 normal;
+	XVector3 tex;//此纹理坐标是
+};
+
 struct XChunk//一个小区块
+{
+	XAnchorPos
+};
+
+struct XChunkArea//共享的顶点数据 的几个区块
 {
 
 };
 
+struct XTerrainArea//一块地图区域
+{
+	
+};
+
 class XTerrain
 {
-
+public:
+	typedef stdext::hash_map<XAnchorPos, XTerrainArea*> PosToTerrAreaMap;
+protected:
+	PosToTerrAreaMap map_pos_to_terrarea;
+	int area_count;//子区域的数量
 };
 
 #endif // XTerrain
