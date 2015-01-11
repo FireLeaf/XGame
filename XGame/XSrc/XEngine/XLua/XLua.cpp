0d310646fd9e1e17728fe3b7b333441892c89a4d
mirchd <mirchd@126.com>
2015-01-11 20:52:04 +0800

    仅添加接口

--- XGame/XSrc/XEngine/XLua/XLua.cpp
+++ XGame/XSrc/XEngine/XLua/XLua.cpp
@@ -13,7 +13,7 @@
 
 CXLua::CXLua()
 {
-
+	
 }
 
 CXLua::~CXLua()
@@ -29,6 +29,18 @@ int CXLua::CreateLuaRuntime()
 		OutputDebug("cannot create state: not enough memory");
 		return EXIT_FAILURE;
 	}
+
+	// ³õÊ¼»¯ Lua ³ÌÐò¿â
+	luaopen_base(m_pLuaState);
+	luaopen_package(m_pLuaState);
+	luaopen_coroutine(m_pLuaState);
+	luaopen_table(m_pLuaState);
+	luaopen_io(m_pLuaState);
+	luaopen_os(m_pLuaState);
+	luaopen_string(m_pLuaState);
+	luaopen_bit32(m_pLuaState);
+	luaopen_math(m_pLuaState);
+	luaopen_debug(m_pLuaState);
 
 	return EXIT_SUCCESS;
 }
